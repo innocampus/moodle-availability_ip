969fa633ea7b855440a27d981f14238197c94f8d
Daniel Fainberg <d.fainberg@tu-berlin.de>
2025-07-31 11:15:23 +0200

    build: bump version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'availability_ip';
-$plugin->version = 2025041600;
+$plugin->version = 2025073100;
 $plugin->requires = 2024042200;  // Moodle 4.4.0+.
 $plugin->maturity = MATURITY_BETA;
-$plugin->release = '0.1.0';
+$plugin->release = '0.2.0';
