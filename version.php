4a2906744b5ab42c2a8422cd5e764a24820fa606
Daniel Fainberg <d.fainberg@tu-berlin.de>
2026-07-29 09:39:21 +0200

    feat: add support for Moodle 5.2

--- version.php
+++ version.php
@@ -31,6 +31,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'availability_ip';
 $plugin->version = 2026022400;
 $plugin->requires = 2025041400;  // Moodle 5.0.0+.
-$plugin->supported = [500, 501];
+$plugin->supported = [500, 502];
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0.0';
